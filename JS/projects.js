03fa9539c9f353ab0e6d1561edefdcff15d7d716
Andrew <andrewcappelli13@gmail.com>
2025-10-16 00:41:48 -0400

    new image video and fix for js for projects page

--- JS/projects.js
+++ JS/projects.js
@@ -7,28 +7,19 @@ function loadProjects() {
       let projectHTML = '';
 
       projects.forEach(project => {
-        // Conditionally include video or image
-        const mediaContent = project.video
-          ? `
-            <video preload="none" loop muted poster="${project.poster}" 
-              onmouseover="this.play()" 
-              onmouseout="this.pause();this.currentTime=0;">
-              <source src="${project.video}" type="video/mp4">
-              Your browser does not support the video tag.
-            </video>`
-          : `<img src="${project.poster}" alt="${project.name}" class="project-image">`;
-
         projectHTML += `
           <div class="project-column">
-            <a href="${project.link}" target="_blank">
-              ${mediaContent}
+          <a href="${project.link}" target="_blank">
+            <video  preload="none" loop muted poster="${project.poster}" onmouseover="this.play()" onmouseout="this.pause();this.currentTime=0;">
+              <source src="${project.video}" type="video/mp4">Your browser does not support the video tag.
+            </video>
             </a>
             <div class="project-label">${project.name}</div>
           </div>
         `;
       });
 
-      container.innerHTML = projectHTML;
+      container.innerHTML = projectHTML; // Insert the generated HTML into the container
     })
     .catch(error => console.error('Error loading projects:', error));
 }
