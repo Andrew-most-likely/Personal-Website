baa11d58cff597209a390ec1607c48d50934029a
Andrew <andrewcappelli13@gmail.com>
2025-10-16 00:22:35 -0400

    script to account for non video project page links

--- JS/projects.js
+++ JS/projects.js
@@ -7,19 +7,28 @@ function loadProjects() {
       let projectHTML = '';
 
       projects.forEach(project => {
+        // Conditionally include video or image
+        const mediaContent = project.video
+          ? `
+            <video preload="none" loop muted poster="${project.poster}" 
+              onmouseover="this.play()" 
+              onmouseout="this.pause();this.currentTime=0;">
+              <source src="${project.video}" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>`
+          : `<img src="${project.poster}" alt="${project.name}" class="project-image">`;
+
         projectHTML += `
           <div class="project-column">
-          <a href="${project.link}" target="_blank">
-            <video  preload="none" loop muted poster="${project.poster}" onmouseover="this.play()" onmouseout="this.pause();this.currentTime=0;">
-              <source src="${project.video}" type="video/mp4">Your browser does not support the video tag.
-            </video>
+            <a href="${project.link}" target="_blank">
+              ${mediaContent}
             </a>
             <div class="project-label">${project.name}</div>
           </div>
         `;
       });
 
-      container.innerHTML = projectHTML; // Insert the generated HTML into the container
+      container.innerHTML = projectHTML;
     })
     .catch(error => console.error('Error loading projects:', error));
 }
